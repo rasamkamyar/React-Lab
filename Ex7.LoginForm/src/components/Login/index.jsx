4d7326f92e59634f3f6e040e4b38b972728c9cc4
rasamkamyar <rasamkamyar@gamil.com>
2024-02-18 14:59:26 +0330

    first and second inputs finished

--- Ex7.LoginForm/src/components/Login/index.jsx
+++ Ex7.LoginForm/src/components/Login/index.jsx
@@ -1,27 +1,73 @@
+import { useRef, useState } from "react";
 import style from "./index.module.css";
 
 function Login() {
+  const [errorPersian, setErrorPersian] = useState("");
+  const [errorEnglish, setErrorEnglish] = useState("");
+  const inputPresianRef = useRef(null);
+  const inputEnglishRef = useRef(null);
+
+  function handlePersianInput() {
+    const inputPersianValue = inputPresianRef.current.value;
+    const persianAlphabet = /^[\u0600-\u06FF\s]+$/;
+
+    if (persianAlphabet.test(inputPersianValue)) {
+      setErrorPersian("");
+    } else {
+      setErrorPersian("لطفا کیبورد خود را در حالت فارسی قرار دهید.");
+    }
+    if (inputPresianRef.current.value === "") {
+      setErrorPersian("نام و نام خانوادگی به فارسی  نمیتواند خالی باشد.");
+    }
+  }
+
+  function handleEnglishInput() {
+    const inputEnglishValue = inputEnglishRef.current.value;
+    const EnglishAlphabet = /^[a-zA-Z]+$/;
+    if (EnglishAlphabet.test(inputEnglishValue)) {
+      setErrorEnglish("");
+    } else {
+      setErrorEnglish("لطفا کیبورد خود را در حالت انگلیسی قرار دهید.");
+    }
+    if (inputEnglishRef.current.value === "") {
+      setErrorEnglish("نام و نام خانوادگی به انگلیسی  نمیتواند خالی باشد.");
+    }
+  }
   return (
     <div className={style.container}>
       <div className={style.rightContainer}>
         <h1>مشخصات فردی</h1>
-        <input
-          className={style.input}
-          type="text"
-          placeholder="نام و نام خانوادگی به فارسی"
-        />
-        <input
-          className={style.input}
-          type="text"
-          placeholder="نام و نام خانوادگی به انگلیسی"
-        />
-        <input className={style.input} type="email" placeholder="ایمیل" />
-        <select
-          className={style.input}
-          style={{ padding: "15px", width: "434px" }}
-        >
-          <option value="">1370</option>
-        </select>
+        <div className={style.nameInput}>
+          <input
+            ref={inputPresianRef}
+            onChange={handlePersianInput}
+            className={`${style.input}  ${errorPersian ? style.redBorder : ""}`}
+            type="text"
+            placeholder="نام و نام خانوادگی به فارسی"
+          />
+          {<p className={style.redText}>{errorPersian}</p>}
+        </div>
+        <div className={style.nameInput}>
+          <input
+            ref={inputEnglishRef}
+            onChange={handleEnglishInput}
+            className={`${style.input}  ${errorEnglish ? style.redBorder : ""}`}
+            type="text"
+            placeholder="نام و نام خانوادگی به انگلیسی"
+          />
+          {<p className={style.redText}>{errorEnglish}</p>}
+        </div>
+        <div>
+          <input className={style.input} type="email" placeholder="ایمیل" />
+        </div>
+        <div>
+          <select
+            className={style.input}
+            style={{ padding: "15px", width: "434px" }}
+          >
+            <option value="">1370</option>
+          </select>
+        </div>
         <button
           style={{
             padding: "15px",
